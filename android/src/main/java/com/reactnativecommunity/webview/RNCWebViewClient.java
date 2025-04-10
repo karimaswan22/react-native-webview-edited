e5370f6c7327bc28bf490469971ad8bfab8cb1c8
unknown <blackaswan22@gmail.com>
2025-04-10 17:00:24 +0100

    first commitsssss

--- android/src/main/java/com/reactnativecommunity/webview/RNCWebViewClient.java
+++ android/src/main/java/com/reactnativecommunity/webview/RNCWebViewClient.java
@@ -38,7 +38,7 @@ import java.util.concurrent.atomic.AtomicReference;
 
 public class RNCWebViewClient extends WebViewClient {
     private static String TAG = "RNCWebViewClient";
-    FLog.e("MainActivity", "lovemorocco");
+    Log.i("MainActivity", "lovemorocco");
     protected static final int SHOULD_OVERRIDE_URL_LOADING_TIMEOUT = 250;
 
     protected boolean mLastLoadFailed = false;
@@ -90,7 +90,7 @@ public class RNCWebViewClient extends WebViewClient {
     public void onPageStarted(WebView webView, String url, Bitmap favicon) {
       super.onPageStarted(webView, url, favicon);
       mLastLoadFailed = false;
-      FLog.e("MainActivity", "lovemorocco");
+      Log.i("MainActivity", "lovemorocco");
 
       RNCWebView reactWebView = (RNCWebView) webView;
       reactWebView.callInjectedJavaScriptBeforeContentLoaded();
